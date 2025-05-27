441f5ba9fb7a3c6fa28e8c9b5fb5e93d750c01d5
kotiko <megasuslik666@gmail.com>
2025-05-27 18:27:39 +0300

    add files

---
+++ main.js
@@ -0,0 +1,69 @@
+gsap.registerPlugin(ScrollTrigger)
+
+function animateRec(index) {
+	gsap.to(`.rec-${index}`, {
+		scrollTrigger: {
+			trigger: `.trigger-${index}`,
+			start: 'top center',
+			end: () => '+=' + window.innerHeight,
+			markers: true,
+			toggleActions: 'play reverse play reverse',
+		},
+		backgroundColor: 'green',
+		width: '300',
+		duration: 1,
+	})
+}
+
+;[1, 2, 3, 4].forEach(animateRec)
+
+// або якщо треба анімувати по різному
+
+// gsap.to('.rec-1', {
+// 	scrollTrigger: {
+// 		trigger: '.trigger-1',
+// 		start: 'top center',
+// 		end: () => '+=' + window.innerHeight,
+// 		markers: true,
+// 		toggleActions: 'play reverse play reverse',
+// 	},
+// 	backgroundColor: 'green',
+// 	width: '300',
+// 	duration: 1,
+// })
+// gsap.to('.rec-2', {
+// 	scrollTrigger: {
+// 		trigger: '.trigger-2',
+// 		start: 'top center',
+// 		end: () => '+=' + window.innerHeight,
+// 		markers: true,
+// 		toggleActions: 'play reverse play reverse',
+// 	},
+// 	backgroundColor: 'green',
+// 	width: '300',
+// 	duration: 1,
+// })
+// gsap.to('.rec-3', {
+// 	scrollTrigger: {
+// 		trigger: '.trigger-3',
+// 		start: 'top center',
+// 		end: () => '+=' + window.innerHeight,
+// 		markers: true,
+// 		toggleActions: 'play reverse play reverse',
+// 	},
+// 	backgroundColor: 'green',
+// 	width: '300',
+// 	duration: 1,
+// })
+// gsap.to('.rec-4', {
+// 	scrollTrigger: {
+// 		trigger: '.trigger-4',
+// 		start: 'top center',
+// 		end: () => '+=' + window.innerHeight,
+// 		markers: true,
+// 		toggleActions: 'play reverse play reverse',
+// 	},
+// 	backgroundColor: 'green',
+// 	width: '300',
+// 	duration: 1,
+// })
